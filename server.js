0ef2a99c64f36ac4a6b1ded100bb8436a401ec51
Aaron Strouse <aaronstrouse@gmail.com>
2018-12-31 16:30:33 -0500

    added messages array to messages endpoint

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var app = express()
 
 app.use(express.static(__dirname))
 
+var messages = [
+  {name: 'Aaron', message: 'hey from Aaron'},
+  {name: 'Susan', message: 'hey from Susan'},
+]
+
 app.get('/messages', (req, res) =>{
-  res.send('hello')
+  res.send(messages)
 }) 
 var server = app.listen(3000, () => {
   console.log('The server is listening on port', server.address().port)
