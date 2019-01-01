c67d740d92b7a0ab4f7e2c7a6e141ddcb6154251
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-01 12:01:58 -0500

    added POST endpoint and module body-parser

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
 
 var messages = [
   {name: 'Aaron', message: 'hey from Aaron'},
@@ -11,6 +14,12 @@ var messages = [
 app.get('/messages', (req, res) =>{
   res.send(messages)
 }) 
+
+app.post('/messages', (req, res) =>{
+  console.log(req.body)
+  res.sendStatus(200)
+}) 
+
 var server = app.listen(3000, () => {
   console.log('The server is listening on port', server.address().port)
 })
