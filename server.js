e77d5255440103f1b291ba3c9deadd91771d3e4d
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-02 17:06:23 -0500

    setting up model and schema for MongoDB

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
 var dburl = 'mongodb://user:dbpassword1@ds147684.mlab.com:47684/node-chat-app'
+
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
+
 var messages = [
   {name: 'Aaron', message: 'hey from Aaron'},
   {name: 'Susan', message: 'hey from Susan'},
@@ -21,9 +27,17 @@ app.get('/messages', (req, res) =>{
 }) 
 
 app.post('/messages', (req, res) =>{
-  messages.push(req.body)
+  var message = new Message(req.body)
+
+  message.save((err) => {
+    if (err)
+      sendStatus(500)
+
+      messages.push(req.body)
   io.emit('message', req.body)
   res.sendStatus(200)
+  })
+
 }) 
 
 
