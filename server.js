c8e65e0992a86e64d1d224b22e35c319cbb97489
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-02 14:16:16 -0500

    added Mongoose and MongoDB

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
+var dburl = 'mongodb://user:dbpassword1@ds147684.mlab.com:47684/node-chat-app'
 var messages = [
   {name: 'Aaron', message: 'hey from Aaron'},
   {name: 'Susan', message: 'hey from Susan'},
@@ -27,6 +29,10 @@ app.post('/messages', (req, res) =>{
 
 io.on('connection', (socket) => {
   console.log('a user connected')
+})
+
+mongoose.connect(dburl, (err)=>{
+  console.log('mongodb connection', err)
 })
 
 var server = http.listen(3000, () => {
